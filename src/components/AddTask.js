feda8a1c768f4a60b0324999b9e9920ce65ce701
Agnieszka Polińska <agapoli@gmail.com>
2021-06-03 17:45:08 +0200

    handle form fields

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,19 +2,41 @@ import { Component } from 'react';
 import './AddTask.css'
 
 class AddTask extends Component {
+    minDate = new Date().toISOString().slice(0,10);
     state = {
         text: "",
         checked: false,
-        date: "2021-06-03",
+        date: this.minDate,
+    }
+
+    handleText = e => {
+        this.setState({
+            text: e.target.value,
+        })
+    }
+
+    handleCheckbox = e => {
+        this.setState({
+            checked: e.target.checked,
+        })
+    }
+
+    handleDate = e => {
+        this.setState({
+            date: e.target.value,
+        })
     }
 
     render() {
+        let maxDate = Number(this.minDate.slice(0,4)) + 1;
+        maxDate = maxDate+"-12-31";
+
         return (
             <form className="form">
-                <label htmlFor="name"><input type="text" placeholder="dodaj zadanie" id="name" value={this.state.text}/></label>
-                <label htmlFor="important"><input type="checkbox" checked={this.state.checked} id="important"/> Priorytet</label>
+                <label htmlFor="name"><input type="text" placeholder="dodaj zadanie" id="name" value={this.state.text} onChange={this.handleText}/></label>
+                <label htmlFor="important"><input type="checkbox" checked={this.state.checked} id="important" onChange={this.handleCheckbox}/> Priorytet</label>
                 <br />
-                <label htmlFor="date">Do kiedy zrobić: <input type="date" id="date" value={this.state.date} min="2021-06-03" max="2022-12-31"/></label>
+                <label htmlFor="date">Do kiedy zrobić: <input type="date" id="date" value={this.state.date} min={this.minDate} max={maxDate} onChange={this.handleDate}/></label>
                 <br />
                 <button>DODAJ</button>
             </form>
