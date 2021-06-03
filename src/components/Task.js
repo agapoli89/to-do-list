366ef89c7b0fccb0a4dddb330555d0a62ea07ed5
Agnieszka Polińska <agapoli@gmail.com>
2021-06-03 16:37:34 +0200

    Add new functionalities:
    mark important tasks in red, add finish date to done tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,17 +2,38 @@ import './Task.css';
 
 const Task = props => {
 
-    const { text, date, id } = props.task;
+    const importantStyle = {
+        color: 'red',
+    }
+    const { text, date, id, active, important, finishDate } = props.task;
 
-    return (
-        <li className="task">
-            <p>
-                <strong>{text}</strong> - do <span>{date} </span>
-                <button onClick={() => props.change(id)}>Zrobione</button>
-                <button onClick={() => props.delete(id)}>x</button>
-            </p>    
-        </li>
-    )
+    if (active) {
+        return (
+            <li className="task">
+                <p>
+                    <strong style={important ? importantStyle : null}>{text}</strong> - do <span>{date} </span>
+                    <button onClick={() => props.change(id)}>Zrobione</button>
+                    <button onClick={() => props.delete(id)}>x</button>
+                </p>    
+            </li>
+        )
+    } else {
+
+        const finish = new Date(finishDate).toLocaleString();
+        return (
+            <li className="task">
+                <p>
+                    <strong>{text}</strong><em> (zrobić do: {date}) </em>
+                    <br/>
+                    - zrobione <span>{finish} </span>
+                    <button onClick={() => props.change(id)}>Cofnij</button>
+                    <button onClick={() => props.delete(id)}>x</button>
+                </p>    
+            </li>
+        )
+    }
+
+    
 }
 
 export default Task;
