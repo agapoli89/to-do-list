149b78508a66fca2ac682d8473d48ab3095c2a61
Agnieszka Polińska <agapoli@gmail.com>
2021-06-03 12:36:48 +0200

    Prepare structure of Task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,17 @@
-const Task = () => {
+import './Task.css';
+
+const Task = props => {
+
+    const { text, date, id } = props.task;
+
     return (
-        <div>Task</div>
+        <li className="task">
+            <p>
+                <strong>{text}</strong> - do <span>{date} </span>
+                <button onClick={() => props.change(id)}>Zrobione</button>
+                <button onClick={() => props.delete(id)}>x</button>
+            </p>    
+        </li>
     )
 }
 
