67bb6b23aeef61c89b9e480f8a5cf0ab47b05de6
Agnieszka Polińska <agapoli@gmail.com>
2021-06-03 12:37:07 +0200

    Prepare structure of TaskList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,13 +1,25 @@
 import Task from './Task';
 
-const TaskList = () => {
+const TaskList = props => {
+
+    const activeTasks = props.tasks.filter(task => task.active);
+    const doneTasks = props.tasks.filter(task => !task.active);
+
     return (
-        <div>
-            <h2>Task List</h2>
-            <Task />
-            <Task />
-            <Task />
-        </div>
+        <>
+            <h1>Zadania do zrobienia</h1>
+            <ul className="active">
+                {activeTasks.map(task => (
+                <Task key={task.id} task={task} delete={props.delete} change={props.change}/>
+                ))}
+            </ul>
+            <h2>Zadania zrobione (1) </h2>
+            <ul className="done">
+                {doneTasks.map(task => (
+                <Task key={task.id} task={task} delete={props.delete} change={props.change}/>
+                ))}
+            </ul>
+        </>
     )
 }
 
